597eed100423174ad6960dfbe3933223b9d5e1fa
Matheus Soares <matheus.soares.8d@gmail.com>
2025-04-27 09:39:35 -0300

    Attempt 1

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const DAILY_FEE = 40;
+  const MID_TERM = 3;
+  const MID_TERM_DISCOUNT = 20;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+
+  const subtotal = DAILY_FEE * days;
+
+  if (days >= LONG_TERM) {
+    return subtotal - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= MID_TERM) {
+    return subtotal - MID_TERM_DISCOUNT;
+  }
+
+  return subtotal;
 }
 
 module.exports = calculateRentalCost;
